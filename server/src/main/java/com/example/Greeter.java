7f74ed492350e32970e02483de1201d11cc20001
fernando perez <fperez.canales@gmail.com>
2018-06-29 11:03:06 -0400

    method final

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
    * @param someone nombre para retornar Hello nombre
    * @return frase completa con saludos
    */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
